c1623f21e999d007266c390a69dd653536e36db3
d1n8 <mr.dinyar@mail.ru>
2025-10-21 21:57:54 +0500

    fix declination rubles function

--- frontend/src/utils/utils.ts
+++ frontend/src/utils/utils.ts
@@ -1,5 +1,6 @@
 export const DeclinationRubles = (num: number): string => {
-    if ([0, 2, 3, 4].includes(num % 10)) return 'рубля';
+    if ([11,12,13,14,15,16,17,18,19].includes(num % 100)) return 'рублей'
+    if ([2, 3, 4].includes(num % 10)) return 'рубля';
     if (num % 10 === 1) return 'рубль';
     return 'рублей'
 }
